56016fc118776a113b4bed0c544097aed01d2b53
vmanidev <vmanidev@outlook.com>
2025-08-19 21:48:19 +0530

    implement add todo functionality

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
 
 export default function App() {
   const [todos, setTodos] = useState([]); // state that manages todos data
+
   return (
     <>
+      <AddTodo setTodos={setTodos} />
       <ul>
         {todos.length > 0 && <TodoList todos={todos} setTodos={setTodos} />}
       </ul>
---
+++ react-todo/src/components/AddTodo.jsx
@@ -0,0 +1,19 @@
+import React, { useRef } from "react";
+
+export default function AddTodo({ setTodos }) {
+  const inputRef = useRef(null);
+
+  const addTodo = () => {
+    if (!inputRef.current.value) return;
+    const todo = inputRef.current.value;
+    setTodos((prev) => [...prev, { id: prev.length + 1, todo }]); // update todo list
+    inputRef.current.value = ""; // reset input field
+  };
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" id="add-todo-input" name="addTodo" />
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
+  );
+}
