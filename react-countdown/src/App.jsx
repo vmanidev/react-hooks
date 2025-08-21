5bd4a093c86dc4ad76f6ab8f054d1304e32ae059
vmanidev <vmanidev@outlook.com>
2025-08-22 00:12:42 +0530

    feat: implement a countdown program that demonstrate useEffect hook

--- react-countdown/src/App.jsx
+++ react-countdown/src/App.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-  return <></>;
+  const [countdown, setCountdown] = useState(20);
+  const [message, setMessage] = useState("useEffect is running...");
+
+  useEffect(() => {
+    if (countdown < 0) return;
+
+    const intervalId = setInterval(
+      () => setCountdown((prev) => prev - 1),
+      1000
+    );
+
+    return () => {
+      clearInterval(intervalId);
+      if (countdown === 0) {
+        setMessage((prev) => "useEffect is finished running...");
+      }
+      console.log("useEffect is finished running...");
+    };
+  }, [countdown]);
+
+  return (
+    <div>
+      <div> {countdown < 0 ? "Time is up!" : countdown}</div>
+      <div>{message}</div>
+      <br />
+      <pre style={{ textWrap: "wrap" }}>
+        <b>useEffect</b> is a React hook that lets you run side effects <i>(like API calls, timers, or event listeners)</i> in 
+        functional components.
+      </pre>
+    </div>
+  );
 }
