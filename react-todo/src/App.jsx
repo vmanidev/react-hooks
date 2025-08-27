2594732592c165ca8bb9104c53347bafe7976b38
vmanidev <vmanidev@outlook.com>
2025-08-28 00:59:22 +0530

    feat: implement welcome ui

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
+import Welcome from "./components/Welcome";
 
 export default function App() {
   const [todos, setTodos] = useState([]); // state that manages todos data
 
+  const renderApp = () => {
+    if (todos.length < 1) {
+      return <Welcome />;
+    } else {
+      return (
+        <ul>
+          <TodoList todos={todos} setTodos={setTodos} />
+        </ul>
+      );
+    }
+  };
   return (
     <div id="main">
       <AddTodo setTodos={setTodos} />
-      <ul>
-        {todos.length > 0 && <TodoList todos={todos} setTodos={setTodos} />}
-      </ul>
+      {renderApp()}
     </div>
   );
 }
---
+++ react-todo/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Welcome() {
+  return (
+    <div className="welcome-message">
+      Welcome 👋
+      <br />
+      Let’s get productive! Add your first task ✅
+    </div>
+  );
+}
