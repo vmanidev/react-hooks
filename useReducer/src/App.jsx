3ac56adcc8476a5d7770ff6caa3afc341f82cea1
vmanidev <vmanidev@outlook.com>
2025-09-25 19:12:27 +0530

    feat: implement todoReducer with add, edit, and delete functionality

--- useReducer/src/App.jsx
+++ useReducer/src/App.jsx
@@ -1,5 +1,33 @@
 import "./index.css";
 
+const todoReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "add": {
+      return {
+        ...state,
+        todos: [...state.todos, { id: state.todos.length + 1, ...payload }],
+      };
+    }
+    case "edit": {
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === payload.id) todo = payload;
+          return todo;
+        }),
+      };
+    }
+    case "delete": {
+      return {
+        ...state,
+        todos: state.todos.filter(({ id }) => id !== payload.id),
+      };
+    }
+  }
+};
+
 export default function App() {
   return <div>App</div>;
 }
