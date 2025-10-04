ec173d23d4795f79d8c8e1d939b333b9eb7ba67c
vmanidev <vmanidev@outlook.com>
2025-10-05 01:04:30 +0530

    feat: add inputs for useId demo

--- useId/src/App.tsx
+++ useId/src/App.tsx
@@ -1,3 +1,21 @@
 export default function App() {
-  return <div>useId hook</div>;
+  return (
+    <>
+      <p>
+        <strong>useId</strong>: a React hook that generates stable and unique
+        dynamic IDs for inputs and other elements, primarily to link labels with
+        inputs and improve accessibility.
+      </p>
+      <form>
+        <div>
+          <label htmlFor="">Username</label>
+          <input id="" name="username" />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input id="" name="password" />
+        </div>
+      </form>
+    </>
+  );
 }
