29db4a0297792f9e86c0abc52edf8cb1c1512870
vmanidev <vmanidev@outlook.com>
2025-10-10 01:04:08 +0530

    feat(hook): implement demo component using forwardRef and useImperativeHandle

--- useImperativeHandle/src/App.tsx
+++ useImperativeHandle/src/App.tsx
@@ -1,3 +1,42 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+export interface CustomInputRef {
+  focusInput: () => void;
+  clearInput: () => void;
+}
+
+const ChildElement = forwardRef<
+  CustomInputRef,
+  React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    focusInput: () => inputRef?.current?.focus(),
+    clearInput: () => {
+      if (inputRef.current) inputRef.current.value = "";
+    },
+  }));
+
+  return (
+    <input
+      ref={inputRef}
+      type="text"
+      name="input-text"
+      placeholder="type something..."
+      {...props}
+    />
+  );
+});
+
 export default function App() {
-  return <div>App</div>;
+  const parentRef = useRef<CustomInputRef>(null);
+
+  return (
+    <div>
+      <ChildElement ref={parentRef} />
+      <button onClick={() => parentRef.current?.focusInput()}>Focus</button>
+      <button onClick={() => parentRef.current?.clearInput()}>Clear</button>
+    </div>
+  );
 }
